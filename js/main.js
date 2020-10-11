9ee456ecd93b0035c0d565099f774c9ef31127c0
sdpereira <sdpereira@misena.edu.co>
2020-10-11 18:27:23 -0500

    metodo SenaSoft

--- js/main.js
+++ js/main.js
@@ -1,51 +1,52 @@
 var app = new Vue({
-    el:'#app',
-    data:{
-        arrayDatos:[],
+    el: '#app',
+    data: {
+        arrayDatos: [],
 
-       producto:null,
-       cantidad:null,
-       total:"",
-       dis:60,
-       disP:85,
-       piña:2500,
-       pera:830,
-       unid:"",
-       msj:""
-  
+        producto: null,
+        cantidad: null,
+        total: "",
+        dis: 60,
+        disP: 85,
+        piña: 2500,
+        pera: 830,
+        unid: "",
+        msj: ""
+
     },
     methods: {
-        procesar:function(){ 
-            
-            if (this.producto=="piña") {
+        procesar: function() {
+
+            if (this.producto == "piña") {
                 this.msj = "piña"
-                this.total = this.cantidad * (this.piña) 
-                this.dis = this.dis - (this.cantidad) 
-                if(this.dis <= 0 ){
-                    alert("excedes el monto, no puedes comprar más piñas " )
+                this.total = this.cantidad * (this.piña)
+                this.dis = this.dis - (this.cantidad)
+                if (this.dis <= 0) {
+                    alert("excedes el monto, no puedes comprar más piñas ")
                     return;
                 }
-                this.arrayDatos.push({ producto:this.producto,cantidad:this.cantidad,total:this.total,unid:this.dis})
-            }            
-                    else{
-                        this.msj= "pera";
-                        this.total = this.cantidad * (this.pera);
-                        this.disP = this.disP - (this.cantidad)
-                        if(this.disP <= 0 ){
-                            alert("excedes el monto, no puedes comprar más peras " )
-                            return;
-                        }
-                        this.arrayDatos.push({producto:this.producto,cantidad:this.cantidad,total:this.total,unid:this.disP})                                    
-                
+                this.arrayDatos.push({ producto: this.producto, cantidad: this.cantidad, total: this.total, unid: this.dis })
+            } else {
+                this.msj = "pera";
+                this.total = this.cantidad * (this.pera);
+                this.disP = this.disP - (this.cantidad)
+                if (this.disP <= 0) {
+                    alert("excedes el monto, no puedes comprar más peras ")
+                    return;
+                }
+                this.arrayDatos.push({ producto: this.producto, cantidad: this.cantidad, total: this.total, unid: this.disP })
+
             }
-        
+
         },
-        hola(){
-            
-        }
-    
+        hola() {
+
+        },
+        SenaSoft() {
+
+        },
+
     },
 
 
 })
-
